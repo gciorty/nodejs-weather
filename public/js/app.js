3ee89d055e8344e134a382d1118ba96b408d9a3d
gciorty <g_ciortea@hotmail.com>
2019-09-21 19:16:14 +0100

    removed consolelog on frontend app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js file loaded')
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const msgOne = document.querySelector('#msg1')
